c45a57b4c02889c8193f2d51bd6b6df4d0ab3a75
Igor Santos <igorsantoscosta@gmail.com>
2022-09-21 16:07:16 -0300

    Add differences dev and prod #5

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -3,9 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const isDevelopment = process.env.NODE_ENV === "development";
 
 export const client = new ApolloClient({
-  uri: !isDevelopment
-    ? process.env.NEXT_PUBLIC_API_URL
-    : "http://localhost:8080",
+  uri: process.env.NEXT_PUBLIC_API_URL,
   headers: {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_ACCESS_TOKEN}`,
   },
